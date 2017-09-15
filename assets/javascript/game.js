660f3f2f44b3140a8892514ce2bfc219a9b94da8
Paul Thorman <paulthorman@Pauls-MacBook-Pro.local>
2017-09-15 13:02:02 -0700

    added audio elements and some polish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,24 @@
 // Hangman Game
 
-
 // Variables
-var wordArray = ["toy story","cars","up","finding nemo","monsters inc","wall e"];
+var wordArray = ["toy story","a bugs life","the incredibles","finding nemo","monsters inc","ratatouille", "monsters university", "finding dory"];
 var open = [];
 var score;
 var guessed = [];
 var attempts;
-var allowedI = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+var allowed = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 
 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+// AUDIO
+
+function playMusic() {
+    music.play();
+}
 
 
 // FUNCTIONS // 
@@ -20,8 +27,8 @@ document.onkeyup = function(event){
     score = 0;
     // randomizing words from array
     var randomWord = wordArray[Math.floor(Math.random()*wordArray.length)];
-    // Allowing total attempts to guess letters 5 greater than count of letters in random word 
-    attempts = randomWord.length + 5; 
+    // Allowing total attempts to guess letters 3 greater than count of letters in random word 
+    attempts = randomWord.length + 3;
     // Display attempts
     document.getElementById('guesses-remaining').innerHTML = attempts;
     for (var i = 0; i < randomWord.length; i++) {
@@ -38,7 +45,7 @@ document.onkeyup = function(event){
         //  Gathering user input from key event and making lower case
         var userInput = String.fromCharCode(event.keyCode).toLowerCase();
         // 
-        if ((allowedI.includes(userInput)) && (guessed.includes(userInput) == false) ) {
+        if ((allowed.includes(userInput)) && (guessed.includes(userInput) == false) ) {
             for (i=0; i<(randomWord.length); i++) {
                 // if the key selected is part of the random word replace the _ with the letter
                 if(randomWord[i] === userInput){
@@ -50,6 +57,8 @@ document.onkeyup = function(event){
             // Subtract an attempt after each key is selected 
             attempts--;
             document.getElementById('guesses-remaining').innerHTML = attempts;
+            // Change image each time incorrect letter is guessed
+
             // Push key event into into guessed array so it can only be selected once
             guessed.push(userInput);
             // Using join method to turn array elements into string
@@ -89,36 +98,6 @@ document.onkeyup = function(event){
                  document.getElementById('currentWord').innerHTML = dash;   
                  }, 500)
             }
-
-            //change hangman photos based on incorrect answer
-                      if('guesses-remaining' === 1){
-                        $('.1').show();
-                        $('.0, .2, .3, .4, .5, .6, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 2){
-                        $('.2').show();
-                        $('.0, .1, .3, .4, .5, .6, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 3){
-                        $('.3').show();
-                        $('.0, .1, .2, .4, .5, .6, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 4){
-                        $('.4').show();
-                        $('.0, .1, .2, .3, .5, .6, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 5){
-                        $('.5').show();
-                        $('.0, .1, .2, .3, .4, .6, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 6){
-                        $('.6').show();
-                        $('.0, .1, .2, .3, .4, .5, .7, .8, .9, .10').hide();
-                      }else if(guessCount === 7){
-                        $('.7').show();
-                        $('.0, .1, .2, .3, .4, .5, .6, .8, .9, .10').hide();
-                      }else if(guessCount === 8){
-                        $('.8').show();
-                        $('.0, .1, .2, .3, .4, .5, .6, .7, .9, .10').hide();
-                      }else if(guessCount === 9){
-                        $('.9').show();
-                        $('.0, .1, .2, .3, .4, .5, .6, .7, .8, .10').hide();
-                      }
 
             }
 
